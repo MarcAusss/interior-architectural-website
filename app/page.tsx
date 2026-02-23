dae23c1fa6aea6be4a5e766a07ff332d8643c6eb
Marc Austin Bonagua <marcaustinbonagua@gmail.com>
2026-02-23 17:23:56 +0800

    asd

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,41 @@
-export default function Home() {
-    return (
-      <div className="">
+import SubFooter from "./components/sub-footer";
+import AboutSection from "./Landing/about";
+import LandingPage from "./Landing/landing";
+import ProcessSection from "./Landing/process";
+import ProjectSection from "./Landing/project";
+import ShowcaseSection from "./Landing/showcase";
+import TeamSection from "./Landing/team";
 
+export default function Home() {
+  return (
+    <>
+      <div className="">
+        <LandingPage />
       </div>
-    );
+
+      <div className="">
+        <ShowcaseSection />
+      </div>
+
+      <div className="">
+        <ProjectSection />
+      </div>
+
+      <div className="">
+        <TeamSection />
+      </div>
+
+      <div className="">
+        <ProcessSection />
+      </div>
+
+      <div className="">
+        <AboutSection />
+      </div>
+
+      <div className="">
+        <SubFooter/>
+      </div>
+    </>
+  );
 }
