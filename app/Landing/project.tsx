fd43fec69715b3c0cdf3126133b495269380e184
Marc Austin Bonagua <marcaustinbonagua@gmail.com>
2026-02-24 13:48:30 +0800

    asdasd

--- app/Landing/project.tsx
+++ app/Landing/project.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+
+
+
 export default function ProjectSection() {
     return (
         <div>
-            
+            <div className="w-full h-[80vh] relative">
+                <Image src="/images/image-2.png" alt="" className="object-cover" fill/>
+            </div>            
         </div>
     );
 }
