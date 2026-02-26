41dc3f1303852a6e9f7814ef52248f5ee35e2ee7
Marc Austin Bonagua <marcaustinbonagua@gmail.com>
2026-02-26 23:05:47 +0800

    asd

--- app/Landing/project.tsx
+++ app/Landing/project.tsx
@@ -9,8 +9,8 @@ export default function ProjectSection() {
             <div className="max-w-365 mx-auto py-10 h-[80vh]">
                 <div className="flex flex-col justify-between relative z-20 h-full">
                     <h1 className="text-white">2/ <span className="text-gray-300">4</span></h1>
-                    <div className="flex">
-                        <div className="flex w-1/2 justify-between border-b border-gray-300">
+                    <div className="flex items-center">
+                        <div className="flex w-1/2 justify-between border-b border-gray-300 h-fit pr-5">
                             <h1 className="my-3 text-gray-300">2017</h1>
                             <h1 className="my-3 text-white">2019</h1>
                         </div>
@@ -26,8 +26,8 @@ export default function ProjectSection() {
                                         defined Renova&apos;s material-lead and timeless approach.
                                     </p>
                                 </div>    
-                                <div className="w-1/2 h-full relative">
-                                    <Image src="/images/image-2.png" alt="" className="object-cover" fill/>
+                                <div className="w-96 h-96 relative">
+                                    <Image src="/images/image 15.png" alt="" className="object-cover" fill/>
                                 </div>
                             </div>
                         </div>
