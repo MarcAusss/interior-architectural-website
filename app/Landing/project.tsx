d72f0e202754c168a1ff1c774af50beee9637653
Marc Austin Bonagua <marcaustinbonagua@gmail.com>
2026-02-26 23:01:55 +0800

    asd

--- app/Landing/project.tsx
+++ app/Landing/project.tsx
@@ -7,7 +7,7 @@ export default function ProjectSection() {
         <div className="w-full h-[80vh] relative">
             <Image src="/images/image-2.png" alt="" className="object-cover" fill/>
             <div className="max-w-365 mx-auto py-10">
-                <div className="flex flex-col justify-between">
+                <div className="flex flex-col justify-between relative z-20">
                     <h1 className="text-white">2/ <span className="text-gray-300">4</span></h1>
                     <div className="flex">
                         <div className="flex w-1/2 justify-between border-b border-gray-300">
